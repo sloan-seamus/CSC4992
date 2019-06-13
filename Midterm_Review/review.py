20fc8affd3980266412fdbcc0a275a0d35df9fa1
seamus <seamus.sloan@gmail.com>
2019-06-13 17:25:06 -0400

    Additions to review.py

--- Midterm_Review/review.py
+++ Midterm_Review/review.py
@@ -23,6 +23,26 @@ print("Print the odd numbers in the test scores list")
 listComp3 = [score for score in testScores if score % 2 == 1]
 print(listComp3)
 
+print(88 in testScores)
+
+a = [(1, 2)]
+b = [3, 4]
+b = b + a
+b = b + a
+a.append(3)
+print(b)
+
+for x in [1, 2, 3]:
+    for y in [3, x]:
+        print(x, y)
+
+
+y = []
+w = 10
+while w > 0:
+    y += [[w]]
+    w /= 2
+print(y)
 
 
 
