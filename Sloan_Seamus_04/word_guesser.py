3d5a924b68d0489461d448d4aa31c50a2498a465
Seamus <fy7487@wayne.edu>
2019-06-24 19:20:27 -0400

    Assignment 4 completed:
    Implemented breezypythongui
    Created a GUI for word guessing
    Implemented word checking for guessed word
    Implemented word scrambling - with random.shuffle

--- Sloan_Seamus_04/word_guesser.py
+++ Sloan_Seamus_04/word_guesser.py
@@ -1,36 +1,65 @@
 #!/usr/bin/env python3
 import random
-import tkinter
 from breezypythongui import EasyFrame
 
+count = 0
+word = ""
+points = 0
 
-infile = open("words.txt", "r")
-lines = infile.readlines()
+
+class WordGuesser(EasyFrame):
+    def __init__(self):
+
+        EasyFrame.__init__(self, title="Word Guesser 1.0")
+
+        self.addLabel(text="Word: ", row=0, column=0)
+        self.actualWordField = self.addTextField(text="", row=0, column=1)
+
+        self.addLabel(text="Guess: ", row=1, column=0)
+        self.guessField = self.addTextField(text="", row=1, column=1)
+
+        self.addButton(text="Guess It!", row=2, column=0, columnspan=2, command=self.checkWord)
+
+        self.addLabel(text="Points: ", row=4, column=0)
+        self.pointField = self.addIntegerField(value=0, row=4, column=1)
+
+    def checkWord(self):
+        if count == 0:
+            self.changeWord()
+
+        elif self.guessField.getText() == word:
+            self.changeWord()
+            self.addPoints()
+            self.guessField.option_clear()
+            self.messageBox(title="Correct!", message="You got it!\n\n+1 Points")
+        else:
+            self.messageBox(title="Incorrect", message="Incorrect Guess, Try Again!")
+
+    def changeWord(self):
+        global word
+        word = self.getWord()
+        shuffled = list(str(word))
+        random.shuffle(shuffled)
+        shuffled = ''.join(shuffled)
+        self.actualWordField.setText(shuffled)
+
+    def addPoints(self):
+        global points
+        points += 1
+        self.pointField.setNumber(points)
+
+    def getWord(self):
+        global count
+        words = open("words.txt")
+        lines = words.readlines()
+        print(len(lines))
+        count += 1
+        if (count - 1) >= len(lines):
+            self.messageBox(title="Complete!", message="You have successfully completed all words!\n\n"
+                                                       "Game will exit now.")
+            exit()
+        else:
+            return lines[count - 1].strip()
 
 
-def __init__(self):
-    """Sets up the window and the widgets."""
-    EasyFrame.__init__(self, title="Tax Calculator")
-
-    # Label and field for the income
-    self.addLabel(text="Income", row=0, column=0)
-    self.incomeField = self.addFloatField(value=0.0, row=0, column=1)
-
-    # Label and field for the number of dependents
-    self.addLabel(text="Dependents", row=1, column=0)
-    self.depField = self.addIntegerField(value=0, row=1, column=1)
-
-    # Label and field for the exemption amount
-    self.addLabel(text="Exemption amount", row=2, column=0)
-    self.exeField = self.addFloatField(value=0.0, row=2, column=1)
-    # The command button
-    self.addButton(text="Compute", row=3, column=0,
-                   columnspan=2, command=self.computeTax)
-
-    # Label and field for the tax
-    self.addLabel(text="Total tax",
-                  row=4, column=0)
-    self.taxField = self.addFloatField(value=0.0,
-                                       row=4,
-                                       column=1,
-                                       precision=2)
+WordGuesser().mainloop()
